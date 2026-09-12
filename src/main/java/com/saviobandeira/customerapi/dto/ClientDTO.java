e2acc0b0fd552b79b3c8143a8634b9c493b3d22b
saviobandeira <savio-bandeira@hotmail.com>
2026-09-12 08:22:02 -0300

    feat: add field validation

--- src/main/java/com/saviobandeira/customerapi/dto/ClientDTO.java
+++ src/main/java/com/saviobandeira/customerapi/dto/ClientDTO.java
@@ -4,13 +4,30 @@ import com.saviobandeira.customerapi.entities.Client;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public class ClientDTO {
-    
+
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Nome é obrigatório")
     private String name;
+
+    @Size(min = 11, max = 11, message = "CPF deve conter 11 dígitos")
     private String cpf;
+
+    @Positive(message = "Renda deve ser maior que zero")
     private Double income;
+
+    @PastOrPresent(message = "Data de nascimento não pode ser futura")
     private LocalDate birthDate;
+
+    @PositiveOrZero(message = "Quantidade de filhos não pode ser negativa")
     private Integer children;
 
     public ClientDTO() {
