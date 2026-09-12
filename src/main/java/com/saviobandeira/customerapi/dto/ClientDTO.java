c68b4db904a4dded18003c8d0e54a668ee649f7b
saviobandeira <savio-bandeira@hotmail.com>
2026-09-12 07:51:52 -0300

    chore: correct indentation

--- src/main/java/com/saviobandeira/customerapi/dto/ClientDTO.java
+++ src/main/java/com/saviobandeira/customerapi/dto/ClientDTO.java
@@ -5,56 +5,56 @@ import com.saviobandeira.customerapi.entities.Client;
 import java.time.LocalDate;
 
 public class ClientDTO {
+    
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
 
-        private Long id;
-        private String name;
-        private String cpf;
-        private Double income;
-        private LocalDate birthDate;
-        private Integer children;
+    public ClientDTO() {
+    }
 
-        public ClientDTO() {
-        }
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
 
-        public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
-            this.id = id;
-            this.name = name;
-            this.cpf = cpf;
-            this.income = income;
-            this.birthDate = birthDate;
-            this.children = children;
-        }
+    public ClientDTO(Client client) {
+        id = client.getId();
+        name = client.getName();
+        cpf = client.getCpf();
+        income = client.getIncome();
+        birthDate = client.getBirthDate();
+        children = client.getChildren();
+    }
 
-        public ClientDTO(Client client) {
-            id = client.getId();
-            name = client.getName();
-            cpf = client.getCpf();
-            income = client.getIncome();
-            birthDate = client.getBirthDate();
-            children = client.getChildren();
-        }
+    public Long getId() {
+        return id;
+    }
 
-        public Long getId() {
-            return id;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public String getCpf() {
+        return cpf;
+    }
 
-        public String getCpf() {
-            return cpf;
-        }
+    public Double getIncome() {
+        return income;
+    }
 
-        public Double getIncome() {
-            return income;
-        }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
 
-        public LocalDate getBirthDate() {
-            return birthDate;
-        }
-
-        public Integer getChildren() {
-            return children;
-        }
+    public Integer getChildren() {
+        return children;
+    }
 }
